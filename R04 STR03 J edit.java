407669e13523451979284870092c83c74a297f9d
lelanddutcher <73373945+lelanddutcher@users.noreply.github.com>
2020-10-25 18:56:03 -0400

    Create R04 STR03 J edit.java

---
+++ R04 STR03 J edit.java
@@ -0,0 +1,5 @@
+BigInteger x = new BigInteger("530500452766");
+byte[] byteArray = x.toByteArray();
+String s = Base64.getEncoder().encodeToString(byteArray);
+byteArray = Base64.getDecoder().decode(s);
+x = new BigInteger(byteArray);
