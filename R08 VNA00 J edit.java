13a06a0922ee3ffbd92a35ace47023e00cd544a3
lelanddutcher <73373945+lelanddutcher@users.noreply.github.com>
2020-10-25 19:08:04 -0400

    Create R08 VNA00 J edit.java

---
+++ R08 VNA00 J edit.java
@@ -0,0 +1,22 @@
+final class ControlledStop implements Runnable {
+  private boolean done = false;
+  
+  @Override public void run() {
+    while (!isDone()) {
+      try {
+        // ...
+        Thread.currentThread().sleep(1000); // Do something
+      } catch(InterruptedException ie) {
+        Thread.currentThread().interrupt(); // Reset interrupted status
+      }
+    }   
+  }
+ 
+  public synchronized boolean isDone() {
+    return done;
+  }
+ 
+  public synchronized void shutdown() {
+    done = true;
+  }
+}
